e9a7ba8d60dad13449e93caad146cf1737e88e2b
Valeriia.Kurylko <reviliya@gmail.com>
2023-02-01 14:03:46 +0100

    basic structure of project component

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -4,6 +4,14 @@ function Projects() {
     return (
         <div className='projects'>
             Projects
+            <div className='tech_menu'>Menu</div>
+            <div className='projects_container'>
+                <div className='single_project'>Project 1</div>
+            </div>
+            <div className='tag_menu'>
+                <div>Tags:</div>
+                <div className='single_tag'>tag1</div>
+            </div>
         </div>
     )
 }
