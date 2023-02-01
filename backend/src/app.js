bad691429bae27a7739a8e867af62aaaffdaf36b
Valeriia.Kurylko <reviliya@gmail.com>
2023-02-01 18:07:35 +0100

    adding firebase

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,36 @@
 import express from "express";
+import { database } from "./firebase";
+import { RequestHandler } from "express";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore/lite";
+
 
 import dotenv from "dotenv";
 dotenv.config();
+
 const EXPRESS_PORT = parseInt(process.env.EXPRESS_PORT ?? "5005", 10);
 
 const app = express();
+
+const port = process.env.EXPRESS_PORT;
+
+app.use(express.json());
+
+app.listen(EXPRESS_PORT, () => {
+  console.log("Express listening on port", EXPRESS_PORT);
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.listen(EXPRESS_PORT, () => {
-  console.log("Express listening on port", EXPRESS_PORT);
-});
+
+const getAllProjects = async (req, res) => {
+  const projectCol = collection(database, "projects");
+  const projectsSnapshot = await getDocs(eventsCol);
+  const projectsList = projectsSnapshot.docs.map((doc) => doc.data());
+  res.json(projectsList);
+};
+
+app.get("/projects", getAllProjects);
+
