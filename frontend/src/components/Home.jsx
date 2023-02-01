0d327710bca975c803b81d0611bd132b9c3fabcc
Valeriia.Kurylko <reviliya@gmail.com>
2023-02-01 13:15:53 +0100

    social icons added with links

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,27 @@
 import '../App.css';
+import linkedin from "../../public/linkedin.png";
+import gitlab from "../../public/gitlab.png";
+import github1 from "../../public/github1.png";
 
 function Home() {
     return (
         <div className='home'>
-            Home component
             <div className='home_desc'>
                 <p>Hello
                     I`m Valeria.</p>
                 <p>I`m a Full Stack Web Developer with marketing background. Creating exiting using experience - my truly passion. </p>
                 <p>Programming Language: JavaScript + TypeScript</p>
+            </div>
+            <div className='social_links'>
+                <a href="https://www.linkedin.com/in/valeria-kurylko/">
+                    <div> <img className='social_icon' src={linkedin} alt="linkedin" /> </div>
+                </a>
+                <a href="https://github.com/kurylko">
+                    <div><img className='social_icon' src={github1} alt="github" /></div>
+                </a>
+                <a href="https://gitlab.com/val_kurylko">
+                    <div><img className='social_icon' src={gitlab} alt="gitlab" /></div>
+                </a>
             </div>
         </div>
     )
--- frontend/src/components/Stack.jsx
+++ frontend/src/components/Stack.jsx
@@ -1,9 +1,32 @@
-import '../App.css'; 
+import '../App.css';
 
 function Stack() {
     return (
-        <div className='stack'> 
-            Stack
+        <div className='stack'>
+            <div className='back_front'>
+                <div className='back'>Backend
+                    <div className='dev_block_back'>
+                        <div>Development:</div>
+                        <div><div>Express</div>
+                            <div>Node.js</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='front'>
+                <div className='front'>Frontend
+                    <div className='dev_block_front'>
+                        <div>Development:</div>
+                        <div>
+                            <div>REACT</div>
+                            <div>Next.js</div>
+                            <div>Vite</div>
+                            <div>CSS</div>
+                            <div>HTML</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
