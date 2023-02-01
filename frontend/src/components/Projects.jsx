9e62f8cca0741c812582a4638dac8b448d76a3c0
Valeriia.Kurylko <reviliya@gmail.com>
2023-02-01 17:08:15 +0100

    picture for projects

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -11,7 +11,10 @@ function Projects() {
                 </ul>
             </div>
             <div className='projects_container'>
-                <div className='single_project'>
+                <div className='single_project' style={{backgroundImage: "url(" + "https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"+")",
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat' }}>
                     <div>name</div>
                     <button className='view_btn'>view</button>
                 </div>
