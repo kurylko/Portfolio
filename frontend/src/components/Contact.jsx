42d903d2f42b97b124f10326e16e35dcb87afc63
Valeriia.Kurylko <reviliya@gmail.com>
2023-02-01 14:38:13 +0100

    basic structure for contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -3,7 +3,24 @@ import '../App.css';
 function Contact() {
     return (
         <div className='contact'>
-             Contact
+            CONTACT
+            <div className='contact_container'>
+                <div className='about'>
+                    <div className='contact_pic'>picture</div>
+                    <div>
+                        <p>Collaboration - key for successsful projects.</p>
+                        <p> Feel free to reach me on LinkedIn or send a direct message.</p>
+                    </div>
+                </div>
+
+                <form className='contact_form'>
+                    <input className='message' type="text" placeholder="Your message"></input>
+                    <input className='name' type="text" placeholder="Your name"></input>
+                    <input className='email' type="text" placeholder="Your email"></input>
+                    <button className='submit_form'>Submit</button>
+                    <span className='btn_span'></span>
+                </form>
+            </div>
         </div>
     )
 }
