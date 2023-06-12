b36eacb2c32407a51cb5c73e6afc521f4325e7c3
kurylko <reviliya@gmail.com>
2023-06-12 21:10:00 +0200

    Home description with pre tag

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -1,1 +1,10 @@
-export const FIRST_SECTION_HEIGHT = 800;
+export const FIRST_SECTION_HEIGHT = 803;
+
+export const HOME_DESC_1 = `Hello,
+I'm Valeria.`
+
+export const HOME_DESC_2 = `I am a Full Stack
+Web Developer with
+Marketing && Communications background.
+Creating exiting user experience - my
+truly passion.`
