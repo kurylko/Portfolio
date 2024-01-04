f7923400a702905c07b66a4e95657622005dc153
kurylko <reviliya@gmail.com>
2024-01-04 07:16:35 +0100

    added dart && flutter

--- frontend/src/components/Stack.jsx
+++ frontend/src/components/Stack.jsx
@@ -1,24 +1,26 @@
 import '../App.css';
 import {DESC_2} from "../consts.js";
-import css from '../assets/icons/css.png';
-import html from '../assets/icons/html.png';
+import css from '../assets/icons/css-100.png';
+import html from '../assets/icons/html-100.png';
 import api from '../assets/icons/api.png';
-import express from '../assets/icons/express-js.png';
-import firebase from '../assets/icons/firebase-logo.png';
-import materialui from '../assets/icons/material-ui.png';
-import mysql from '../assets/icons/mysql.png';
+import express from '../assets/icons/express-100.png';
+import firebase from '../assets/icons/firebase-96.png';
+import materialui from '../assets/icons/material-ui.256.png';
+import mysql from '../assets/icons/mysql-100.png';
 import nextjs from '../assets/icons/next-js-logo.png';
-import node from '../assets/icons/node-logo.png';
-import react from '../assets/icons/react.png';
-import sass from '../assets/icons/sass.png';
+import node from '../assets/icons/nodejs-96.png';
+import react from '../assets/icons/react-128.png';
+import sass from '../assets/icons/sass-100.png';
 import tailwind from '../assets/icons/tailwind-logo.png';
 import vite from '../assets/icons/vite-logo.png';
-import jsicon from '../assets/iconsjavascript.png';
-import tsicon from '../assets/iconstypescript.png';
+import jsicon from '../assets/icons/javascript-100.png';
+import tsicon from '../assets/icons/typescript-100.png';
+import dart from '../assets/icons/dart-2.png';
+import flutter from '../assets/icons/flutter-100.png';
 
 function Stack() {
 
-    const iconsArray = [jsicon, tsicon, css, html, react, nextjs, api, materialui, sass, tailwind, vite, node, express, mysql, firebase];
+    const iconsArray = [jsicon, tsicon, css, html, react, nextjs, api, materialui, sass, tailwind, vite, node, express, mysql, firebase, dart, flutter];
 
     return (
         <div className='stack'>
