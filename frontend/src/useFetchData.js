7d63c526c0d2b59769698e5bb2793fcbb4184e6c
kurylko <reviliya@gmail.com>
2024-08-22 13:20:12 +0200

    fetching projects from db and rendering them

--- frontend/src/useFetchData.js
+++ frontend/src/useFetchData.js
@@ -4,18 +4,21 @@ import { db } from './firebase';
 
 const useFetchData = () => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true);
             const querySnapshot = await getDocs(collection(db, "projects"));
             const items = querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}));
             setData(items);
+            setLoading(false);
         }
         fetchData();
     }, []);
-    console.warn('data from Firebase', data);
+    console.log('data from Firebase', data, loading);
 
-    return data;
+    return {data, loading};
 }
 
 export default useFetchData;
