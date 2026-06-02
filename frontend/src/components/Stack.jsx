48c2a420dddc0233450a81d0a33044fa2baa42e9
kurylko <reviliya@gmail.com>
2026-06-02 22:21:34 +0200

    cv upd

--- frontend/src/components/Stack.jsx
+++ frontend/src/components/Stack.jsx
@@ -1,7 +1,7 @@
 import '../App.css';
-import {DESC_2} from "../consts.js";
-import css from '../assets/icons/css-100.png';
-import html from '../assets/icons/html-100.png';
+import { DESC_2 } from '../consts.js';
+import entity from '../assets/icons/entity.jpg';
+import kendo from '../assets/icons/kendo.png';
 import api from '../assets/icons/api.png';
 import express from '../assets/icons/express-100.png';
 import firebase from '../assets/icons/firebase-96.png';
@@ -13,32 +13,82 @@ import react from '../assets/icons/react-128.png';
 import sass from '../assets/icons/sass-100.png';
 import tailwind from '../assets/icons/tailwind-logo.png';
 import vite from '../assets/icons/vite-logo.png';
-import jsicon from '../assets/icons/javascript-100.png';
 import tsicon from '../assets/icons/typescript-100.png';
 import dart from '../assets/icons/dart-2.png';
 import flutter from '../assets/icons/flutter-100.png';
 import postgresql from '../assets/icons/postgresql-100.png';
 import redux from '../assets/icons/redux.png';
+import csharp from '../assets/icons/c-sharp.jpg';
+import neticon from '../assets/icons/neticon.png';
+import signalr from '../assets/icons/signalr.png';
+import aspnet from '../assets/icons/aspnet.svg';
+import docker from '../assets/icons/docker.png';
+import reactflow from '../assets/icons/reactflow.png';
+import cypress from '../assets/icons/cypress.png';
+import tanstack from '../assets/icons/tanstack.svg';
+import aspire from '../assets/icons/aspire.jpg';
+
+
 
 function Stack() {
+  // const iconsArray = [tsicon, react, nextjs, api, materialui, sass, tailwind, vite, node, express, mysql, postgresql,firebase, redux, dart, flutter, csharp, neticon, signalr, aspnet, docker, reactflow];
+  const iconsArray = [
+    // ================= FRONTEND =================
+    { icon: tsicon, label: 'TypeScript' },
+    { icon: react, label: 'React' },
+    { icon: nextjs, label: 'Next.js' },
+    { icon: redux, label: 'Redux' },
+    { icon: vite, label: 'Vite' },
+    { icon: tanstack, label: 'Tanstack Query' },
+    { icon: materialui, label: 'Material UI' },
+    { icon: tailwind, label: 'Tailwind' },
+    { icon: kendo, label: 'Telerik Kendo' },
+    { icon: sass, label: 'Sass' },
+    { icon: reactflow, label: 'React Flow' },
 
-    const iconsArray = [jsicon, tsicon, css, html, react, nextjs, api, materialui, sass, tailwind, vite, node, express, mysql, postgresql,firebase, redux, dart, flutter];
+    // ================= BACKEND =================
+    { icon: node, label: 'Node.js' },
+    { icon: express, label: 'Express' },
+    { icon: api, label: 'API/Swagger' },
+    { icon: aspnet, label: 'ASP.NET' },
+    { icon: csharp, label: 'C#' },
+    { icon: neticon, label: '.NET' },
+    { icon: signalr, label: 'SignalR' },
+    { icon: entity, label: 'Entity Framework' },
 
-    return (
-        <div className='stack'>
-            MY STACK
-            <div className='stack-container'>
-                <h1 className='desc-2 heading-small'>{DESC_2}</h1>
-                <div className='stack-list'>
-                    {iconsArray.map((icon, index)  =>
-                        <div className='stack-item' key={index}>
-                            <img className='stack-icon' src={icon} alt={icon}/>
-                        </div>
-                    )}
-                </div>
+    // ================= DATABASE =================
+    { icon: mysql, label: 'MySQL' },
+    { icon: postgresql, label: 'PostgreSQL' },
+    { icon: firebase, label: 'Firebase' },
+
+    // ================= MOBILE =================
+    { icon: dart, label: 'Dart' },
+    { icon: flutter, label: 'Flutter' },
+
+    // ================= DEVOPS =================
+    { icon: docker, label: 'Docker' },
+    { icon: aspire, label: '.NET Aspire' },
+
+    // ================= OTHER =================
+    { icon: cypress, label: 'Cypress' },
+  ];
+
+  return (
+    <div className="stack">
+      MY STACK
+      <div className="stack-container">
+        <h1 className="desc-2 heading-small">{DESC_2}</h1>
+        <div className="stack-list">
+          {iconsArray.map((item, index) => (
+            <div className="stack-item" key={index}>
+              <img className="stack-icon" src={item.icon} alt={item.icon} />
+                <span className="stack-label">{item.label}</span>
             </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Stack
+export default Stack;
